03c2d63716d16bf414b95fd1bffb1126ff6b260d
Bas Bossink <bas.bossink@gmail.com>
2021-11-10 00:22:37 +0100

    Implement commandline processing

--- main.go
+++ main.go
@@ -8,12 +8,24 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"text/tabwriter"
 	"time"
 )
 
 const (
-	timeEncodedSize = 4
-	bufSize         = 8 * 1024
+	timeEncodedSize            = 4
+	bufSize                    = 8 * 1024
+	sunDataDir                 = ".sun.d"
+	sunDataFileTimestampFormat = "2006"
+	sunDataFileExtension       = ".sun"
+	tagPrefix                  = "@"
+	dateFormat                 = "2006-01-02"
+	timeFormat                 = "15:04:05"
 )
 
 var (
@@ -40,21 +52,18 @@ func NewReader(r io.Reader) (*entryReader, error) {
 }
 
 func (reader *entryReader) Read() (*entry, error) {
-	fmt.Printf("reader.toProcess %v\n", reader.toProcess)
+	if len(reader.toProcess) == 0 {
+		return &entry{}, io.EOF
+	}
 	sizeLen := reader.toProcess[len(reader.toProcess)-1]
-	fmt.Println("sizeLen", sizeLen)
 	sansLen := reader.toProcess[:len(reader.toProcess)-1]
 	varintStart := len(sansLen) - int(sizeLen)
-	fmt.Println("varintsStart", varintStart)
 	varintSlice := sansLen[varintStart:]
-	fmt.Printf("varintSlice %v\n", varintSlice)
 	dec, n := binary.Uvarint(varintSlice)
 	if n <= 0 {
 		return &entry{}, ErrFileCorrupt
 	}
-	fmt.Println("dec", dec, "n", n)
 	sansSize := sansLen[varintStart-int(dec) : varintStart]
-	fmt.Printf("sansSize %v\n", sansSize)
 	rr := bytes.NewReader(sansSize)
 	decoder := gob.NewDecoder(rr)
 	var result entry
@@ -63,38 +72,132 @@ func (reader *entryReader) Read() (*entry, error) {
 		return &entry{}, err
 	}
 	reader.toProcess = reader.toProcess[:len(reader.toProcess)-int(dec)-n-1]
-	fmt.Printf("new toProcess %v\n", reader.toProcess)
 	return &result, nil
 }
 
-func (entry *entry) Write(w io.Writer) (int, error) {
+func (entry *entry) Write(w io.Writer) error {
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
 	err := enc.Encode(entry)
 	if err != nil {
-		return 0, err
+		return err
 	}
 	size := uint64(buf.Len())
-	fmt.Printf("buf %v\n", buf.Bytes())
 	lenBuf := make([]byte, binary.MaxVarintLen64)
 	n := binary.PutUvarint(lenBuf, size)
-	fmt.Printf("n %v, size %v, lenBuf %v\n", n, size, lenBuf)
 	reduced := lenBuf[:n]
-	fmt.Printf("reduced %v\n", reduced)
 	if written, errwr := buf.Write(reduced); errwr != nil {
-		return 0, errwr
+		return errwr
 	} else if written != n {
-		return 0, ErrInsufficientSize
+		return ErrInsufficientSize
 	}
 	if written, errwr := buf.Write([]byte{byte(n)}); errwr != nil {
-		return 0, errwr
+		return errwr
 	} else if written != 1 {
-		return 1, ErrInsufficientSize
+		return ErrInsufficientSize
 	}
-	fmt.Printf("buf %v\n", buf)
-	return w.Write(buf.Bytes())
+	written, err := w.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	if written == 0 {
+		return ErrInsufficientSize
+	}
+	return nil
+}
+
+func ensureDataDir() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	dataDir := filepath.Join(home, sunDataDir)
+	err = os.MkdirAll(dataDir, 0700)
+	if err != nil {
+		return "", err
+	}
+	return dataDir, nil
+}
+
+func printLastEntries(dataDir string) {
+	filename := calculateFilename(dataDir)
+	f, err := os.OpenFile(filename, os.O_RDONLY, 0600)
+	if err != nil {
+		fmt.Errorf("could not open file %w\n", err)
+	}
+	defer f.Close()
+	er, err := NewReader(f)
+	if err != nil {
+		fmt.Errorf("could not create entry reader", err)
+	}
+	w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', tabwriter.Debug)
+	prevDate := ""
+	for entry, err := er.Read(); err != io.EOF; entry, err = er.Read() {
+		curDate := entry.CreatedAt.Format(dateFormat)
+		if prevDate == "" {
+			prevDate = curDate
+		}
+		if prevDate != curDate {
+			fmt.Fprintln(w, "\t\t\t\t\t")
+		}
+		fmt.Fprintln(
+			w,
+			fmt.Sprintf(
+				"\t %s\t %s\t %s\t %s\t",
+				entry.CreatedAt.Format(dateFormat),
+				entry.CreatedAt.Format(timeFormat),
+				strings.Join(entry.Tags, " "),
+				entry.Note))
+		prevDate = curDate
+	}
+	w.Flush()
+}
+
+func writeNewEntry(dataDir string, args []string) {
+	filename := calculateFilename(dataDir)
+	f, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		fmt.Errorf("could not open file %w\n", err)
+	}
+	defer f.Close()
+	entry := convertArgsToEntry(args)
+	err = entry.Write(f)
+	if err != nil {
+		fmt.Errorf("could not write entry %w\n", err)
+	}
+}
+
+func calculateFilename(dataDir string) string {
+	filename := filepath.Join(dataDir, fmt.Sprintf("%s%s", time.Now().Format(sunDataFileTimestampFormat), sunDataFileExtension))
+	return filename
+}
+
+func convertArgsToEntry(args []string) entry {
+	tags := make([]string, 0, len(args))
+	nonTagfields := make([]string, 0, len(args))
+	for _, arg := range args {
+		for _, field := range strings.Fields(arg) {
+			if strings.HasPrefix(field, tagPrefix) {
+				tags = append(tags, strings.TrimPrefix(field, tagPrefix))
+			} else {
+				nonTagfields = append(nonTagfields, field)
+			}
+		}
+	}
+	sort.Strings(tags)
+	note := strings.Join(nonTagfields, " ")
+	entry := entry{Note: note, Tags: tags, CreatedAt: time.Now()}
+	return entry
 }
 
 func main() {
-	fmt.Printf("%v", entry{"First note", time.Now(), []string{"test", "ok"}})
+	dataDir, err := ensureDataDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(os.Args) > 1 {
+		writeNewEntry(dataDir, os.Args[1:])
+	} else {
+		printLastEntries(dataDir)
+	}
 }
