3a153b44893c1194e1ca9942a39e5866c2a8f2e7
Bas Bossink <bas.bossink@gmail.com>
2021-12-01 19:43:38 +0100

    Use shellcheck to improve release shell script

--- release.sh
+++ release.sh
@@ -1,27 +1,30 @@
 #! /usr/bin/env sh
-version=`git describe --tags --exact-match`
-hash=`git rev-parse HEAD`
-ldflags="-s -w -X main.Version=${version} -X main.CommitHash=${hash}"
+current_wd=$(pwd)
 build_dir="build"
+trap 'cd ${current_wd}' EXIT
+version=$(git describe --tags --exact-match)
+hash=$(git rev-parse HEAD)
+ldflags="-s -w -X main.Version=${version} -X main.CommitHash=${hash}"
 readme="README.adoc"
 license="LICENSE"
 prog="sun"
-for line in `go tool dist list`; do
-  GOOS=$(cut -d'/' -f1 <<< $line)
-  GOARCH=$(cut -d'/' -f2 <<< $line)
+for line in $(go tool dist list); do
+  GOOS="${line%/*}"
+  GOARCH="${line#*/}"
   extension=""
-  if [ $GOOS == "windows" ]; then
+  if [ "${GOOS}" = "windows" ]; then
     extension=".exe"
   fi
-  if [ $GOOS == "ios" -o $GOOS == "android" ]; then
+  if [ "${GOOS}" = "ios" ] || [ "${GOOS}" = "android" ] ; then
     continue
   fi
   subdir="${prog}_${GOOS}_${GOARCH}"
   output_file_name="${build_dir}/${subdir}/${prog}${extension}"
-  env GOOS=${GOOS} GOARCH=${GOARCH} CGO_ENABLED=0 go build -ldflags "${ldflags}" -o "${output_file_name}"
-  pushd ${build_dir}
+  env "GOOS=${GOOS}" "GOARCH=${GOARCH}" "CGO_ENABLED=0" go build -ldflags "${ldflags}" -o "${output_file_name}"
+  cd "${build_dir}" || exit 
   ln -s "../../${readme}" "${subdir}/${readme}"
   ln -s "../../${license}" "${subdir}/${license}"
-  zip -9 -r "${subdir}.zip" ${subdir}
-  popd
+  zip -9 -r "${subdir}.zip" "${subdir}"
+  cd "${current_wd}" || exit 
 done
+
